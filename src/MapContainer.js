736935396ee2b78a135ca4fc6a00e1604d250e7f
Emily Rapport <emilyrapport16@gmail.com>
2020-03-03 22:30:11 -0600

    figured out how to visualize geojson

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-import ReactMapboxGl, { Layer, Feature, ZoomControl } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer,
+                        Feature,
+                        ZoomControl,
+                        GeoJSONLayer } from 'react-mapbox-gl';
 
 //CHANGE locally if you want to hit production server Instead
 // TODO: change this to read froma config file
@@ -27,16 +30,30 @@ class MapContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = { circleLoc: MILWAUKEE_CENTER,
-                   zoom: 15};
+                   zoom: 15,
+                   geoJson: {"bbox": [-87.97759552015516, 43.03983160295481, -87.97532666935352, 43.03984560257128], "type": "FeatureCollection", "features": [{"id": "0", "bbox": [-87.97759552015516, 43.03983160295481, -87.97532666935352, 43.03984560257128], "type": "Feature", "geometry": {"type": "LineString", "coordinates": [[-87.97532666935352, 43.03983160295481], [-87.9757795950501, 43.039833603021016], [-87.97714151976831, 43.03984260245887], [-87.97714151976831, 43.03984260245887], [-87.97759552015516, 43.03984560257128]]}, "properties": {}}]}};
   }
 
-  handleMove = () => {
-    var currCircleLocX = this.state.circleLoc[0] - .001;
-    this.setState({ circleLoc: [currCircleLocX, 43.0389]});
+  handleMove = (m) => {
+    //this.getData(m)
   }
 
   handleZoom = (m) => {
     this.setState({ zoom: m.getZoom()})
+  }
+
+  getData = (m) => {
+    var newBounds = m.getBounds().toArray();
+    console.log(newBounds);
+    var req = predsApi + "?lat1=" + newBounds[0][1] + "&long1=" + newBounds[0][0]
+               + "&lat2=" + newBounds[1][1] + "&long2=" + newBounds[1][0];
+
+    axios.get(req)
+      .then(res => {
+        var results = res.data;
+        console.log(results)
+        this.setState({ geoJson: results });
+      })
   }
 
   render() {
@@ -48,13 +65,12 @@ class MapContainer extends React.Component {
             }}
             center={MILWAUKEE_CENTER}
             zoom={[this.state.zoom]}
-            maxBounds={MILWAUKEE_BOUNDS}
+            maxBounds = {MILWAUKEE_BOUNDS}
             onMoveEnd = {this.handleMove}
-            onZoomEnd = {this.handleZoom}
-            >
+            onZoomEnd = {this.handleZoom}>
               <ZoomControl/>
-              <Layer type="circle" id="marker" paint={{"circle-radius": 30}}>
-                <Feature coordinates={this.state.circleLoc} />
+              <Layer type="line" id="sidewalk" paint={{"line-width": 3, "line-color": '#bc13fe'}}>
+                <Feature coordinates={this.state.geoJson.features[0].geometry.coordinates} />
               </Layer>
            </Map>
   }
