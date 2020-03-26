475a661395d84481bc434cc2304d52902a481249
Anu Sankar <anu.k.sankar@berkeley.edu>
2020-03-26 09:21:46 -0500

    added bottom navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from "react-dom";
-import MapContainer from './MapContainer.js'
+import MapContainer from './MapContainer.js';
+import BottomNavBar from './BottomNavBar.js';
 
 function App() {
   return (
     <div>
       <MapContainer />
+      <BottomNavBar />
     </div>
   );
 }
---
+++ src/BottomNavBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import HomeIcon from '@material-ui/icons/Home';
+import AddContentIcon from '@material-ui/icons/AddCircleOutline';
+
+const useStyles = makeStyles({
+  root: {
+    width: 1500,
+  },
+});
+
+export default function BottomNavBar() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      
+      <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Contribute" icon={<AddContentIcon />} />
+    </BottomNavigation>
+  );
+}
