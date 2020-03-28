8a35f23671f45497bfa69d1e9799ad8b8cd3911b
Anu Sankar <anu.k.sankar@berkeley.edu>
2020-03-28 14:50:27 -0500

    added navigation bars and resizing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { render } from "react-dom";
 import MapContainer from './MapContainer.js';
 import BottomNavBar from './BottomNavBar.js';
+import TopAppBar from './TopAppBar.js';
 
 function App() {
   return (
     <div>
+      <TopAppBar />
       <MapContainer />
       <BottomNavBar />
     </div>
+    
   );
 }
 
--- src/BottomNavBar.js
+++ src/BottomNavBar.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+//import RestoreIcon from '@material-ui/icons/Restore';
+//import FavoriteIcon from '@material-ui/icons/Favorite';
+//import LocationOnIcon from '@material-ui/icons/LocationOn';
 import HomeIcon from '@material-ui/icons/Home';
+import AppsIcon from '@material-ui/icons/Apps';
 import AddContentIcon from '@material-ui/icons/AddCircleOutline';
 
-const useStyles = makeStyles({
+// resizes the navigation bar to fit the screen size
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: 1500,
+    flexGrow: 1,
   },
-});
+}));
 
 export default function BottomNavBar() {
   const classes = useStyles();
@@ -29,8 +31,7 @@ export default function BottomNavBar() {
     >
       
       <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Favorite Apps" icon={<AppsIcon />} />
       <BottomNavigationAction label="Contribute" icon={<AddContentIcon />} />
     </BottomNavigation>
   );
